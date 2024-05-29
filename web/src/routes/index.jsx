87fa20b833053bb7a6235d2702eb65d5331edd2d
Willian <willian.dev.ferreira@gmail.com>
2024-05-29 14:27:59 -0300

    refactor: tokens cookie

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -7,9 +7,18 @@ import { AuthRoutes } from './auth.routes';
 import { CustomerRoutes } from './customer.routes';
 import { SaleRoutes } from './sale.routes';
 import { USER_ROLE } from '../utils/roles';
+import { useEffect } from 'react';
 
 export function Routes() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
+
+  useEffect(() => {
+    api.get('/users/validated').catch((error) => {
+      if(error.response?.status === 401){
+        signOut()
+      }
+    })
+  }, [])
 
   function AccessRoute(){
     switch(user.role){
