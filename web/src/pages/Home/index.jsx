86ed8dbed84a1bb6a8254de0b90713b93a9f423d
Willian <willian.dev.ferreira@gmail.com>
2024-05-29 12:31:53 -0300

    feat: validation users

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -3,16 +3,20 @@ import { FiTruck, FiTag, FiShoppingCart } from 'react-icons/fi';
 import { Container } from "./styles";
 import { Feature } from '../../components/Feature';
 import { Header } from '../../components/Header';
+import { useAuth } from '../../hooks/auth';
+import { USER_ROLE } from '../../utils/roles';
 
-export function Home() {
+export function Home() {  
+  const { user } = useAuth();
   return (
     <Container>
       <Header />
 
       <main>
         <Feature title="Produto" icon={FiTag} to="/product" />
-        <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />
-        <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />
+
+        {[USER_ROLE.ADMIN].includes(user.role) && <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />}
+        {[USER_ROLE.ADMIN, USER_ROLE.SALE].includes(user.role) && <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />}
       </main>
     </Container>
   )
--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -2,15 +2,31 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { useAuth } from "../hooks/auth";
 
-import { AppRoutes } from './app.routes';
+import { AdminRoutes } from './admin.routes';
 import { AuthRoutes } from './auth.routes';
+import { CustomerRoutes } from './customer.routes';
+import { SaleRoutes } from './sale.routes';
+import { USER_ROLE } from '../../ultils/roles';
 
 export function Routes() {
   const { user } = useAuth();
 
+  function AccessRoute(){
+    switch(user.role){
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes />;
+      case USER_ROLE.CUSTOMER:
+        return <CustomerRoutes />;
+      case USER_ROLE.SALE:
+        return <SaleRoutes />;
+      default: 
+        return <CustomerRoutes/>;
+    }
+  }
+
   return (
     <BrowserRouter>
-      {user ? <AppRoutes /> : <AuthRoutes />}
+      {user ? <AccessRoute /> : <AuthRoutes />}
     </BrowserRouter>
   );
 }
