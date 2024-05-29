2c2b32b8cfe17337e4a234d95048ef034f1e0a91
Willian <willian.dev.ferreira@gmail.com>
2024-05-29 11:27:51 -0300

    feat: updates sales

--- api/src/database/knex/migrations/20230811164833_createUsers.js
+++ api/src/database/knex/migrations/20230811164833_createUsers.js
@@ -4,7 +4,7 @@ exports.up = knex => knex.schema.createTable("users", table => {
   table.text("email").notNullable();
   table.text("password").notNullable();
 
-  table.enum('role', ["admin", "customer"], { useNative: true, enumName: "roles" })
+  table.enum('role', ["admin", "customer", "sale"], { useNative: true, enumName: "roles" })
   .notNullable().default("customer");
 
   table.timestamp("created_at").default(knex.fn.now());
